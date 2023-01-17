20b691e1feea75ddd6fbb862ac2afc246c648108
CosmoBlack <soulker_mozyr@yahoo.com>
2023-01-17 12:55:11 +0200

    Task 4

--- src/Homework.java
+++ src/Homework.java
@@ -2,20 +2,24 @@ public class Homework {
 
     public static void main(String[] args) {
 
-        short carSpeed1 = 68;
-        short carSpeed2 = 59;
+        byte ageHuman1 = 3;
+        byte ageHuman2 = 10;
+        byte ageHuman3 = 19;
+        byte ageHuman4 = 26;
 
-        if (carSpeed1 > 60) {
-            System.out.println("Если скорость " + carSpeed1 + ", то придется заплатить штраф");
-        } else {
-            System.out.println("Если скорость " + carSpeed1 + ", то можно ездить спокойно");
+        if (ageHuman1 >= 2 && ageHuman1 <= 6) {
+            System.out.println("Если возраст человека равен " + ageHuman1 + ", то ему нужно ходить в детский сад.");
+        }
+        if (ageHuman2 >= 7 && ageHuman2 <= 18) {
+            System.out.println("Если возраст человека равен " + ageHuman2 + ", то ему нужно ходить в школу.");
+        }
+        if (ageHuman3 > 18 && ageHuman3 < 24) {
+            System.out.println("Если возраст человека равен " + ageHuman3 + ", то ему нужно ходить в в университет.");
+        }
+        if (ageHuman4 > 24) {
+            System.out.println("Если возраст человека равен " + ageHuman4 + ", то ему нужно ходить на работу.");
         }
 
-        if (carSpeed2 > 60) {
-            System.out.println("Если скорость " + carSpeed2 + ", то придется заплатить штраф");
-        } else {
-            System.out.println("Если скорость " + carSpeed2 + ", то можно ездить спокойно");
-        }
 
     }
 
